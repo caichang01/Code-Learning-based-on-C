45d134641ee783f7b15653685be296ae34e66faa
caichang01 <cmx_as@hotmail.com>
2017-10-18 18:23:11 +0800

    增加数据结果，顺序存储结构实现方式

--- C-learning/C-learning/源.cpp
+++ C-learning/C-learning/源.cpp
@@ -1,6 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INIT_SIZE 10//初始化表长
+#define INCREMENT_SIZE 5//分配增量
+
+typedef int Status;
+typedef int Elemtype;
+
+typedef struct
+{
+	Elemtype *elem;
+	int length;
+	int size;
+}SqList;
+
+Status InitList(SqList *L)
+{
+
+}
 
 int main()
 {
-	
+		
 }
