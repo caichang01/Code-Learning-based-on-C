8bd40825e84764a55bf0666340f939ece9d734d6
caichang01 <cmx_as@hotmail.com>
2017-10-23 09:51:36 +0800

    增加单链表实现代码

--- C-learning/C-learning/线性表.cpp
+++ C-learning/C-learning/线性表.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>http://bitren.org/
+#include<stdio.h>
 #include<stdlib.h>
 
 #define TRUE 1
---
+++ C-learning/C-learning/链表.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
